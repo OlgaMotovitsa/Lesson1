1c4dfb65b67e62d49b9c4a96187ea5334fcc445d
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-01-14 17:26:02 +0300

    add Ex006_Task

---
+++ Example005LovelyUser/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if (username == "Юля")
+{
+    Console.WriteLine("Ура! Привет Юленька");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
