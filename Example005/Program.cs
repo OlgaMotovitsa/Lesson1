524718af6f22ebaa0b727a192f057b9149533dc4
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-01-14 16:04:55 +0300

    add Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberA = new Random(). Next(1 , 10);
+Console.WriteLine("1е число");
+Console.WriteLine(numberA);
+int numberB = new Random(). Next(1 , 10);
+Console.WriteLine("2е число");
+Console.WriteLine(numberB);
+Console.WriteLine("сумма");
+Console.WriteLine(numberA + numberB);
