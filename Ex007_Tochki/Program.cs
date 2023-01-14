2f61db69fb110f8e1050be8f3b4e2a856567fae9
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-01-14 18:49:35 +0300

    add Ex007_Tocki

---
+++ Ex007_Tochki/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.Clear();
+//Console.Set.CursorPosition(10, 4);
+//Console.WriteLine(" + ");
+
+int xa = 10, ya = 1, 
+    xb = 1, yb = 7,    
+    xc = 20, yc = 7;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+int count = 0;
+while(count < 10)
+{
+int what = new Random().Next(0, 3);
+}
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+int count = 0;
+while(count < 10)
+{
+int what = new Random().Next(0, 3);
+if (what == 0)
+{
+x = (x + xa) / 2;
+y = (y + ya) / 2;
+}
+
+{
+int what = new Random().Next(0, 3);
+if (what == 0)
+{
+x = (x + xa) / 2;
+y = (y + ya) / 2;
+
+}
+if (what == 1)
+{
+x = (x + xb) / 2;
+y = (y + yb) / 2;
+}
+if (what == 2)
+{
+x = (x + xс) / 2;
+y = (y + yс) / 2;
+}
