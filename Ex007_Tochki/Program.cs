59f7f2a32045944bdb1ed47efceefd1357f58428
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-01-16 14:58:42 +0300

    add changes Ex007_Tocki 2

--- Ex007_Tochki/Program.cs
+++ Ex007_Tochki/Program.cs
@@ -3,51 +3,42 @@
 //Console.WriteLine(" + ");
 
 int xa = 10, ya = 1, 
-    xb = 1, yb = 7,    
-    xc = 20, yc = 7;
+    xb = 1, yb = 10,    
+    xc = 20, yc = 10;
 
-Console.Set.CursorPosition(xa, ya);
-Console.WriteLine("+");
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("*");
 
-Console.Set.CursorPosition(xb, yb);
-Console.WriteLine("+");
-
-int x = xa, y = xb;
-int count = 0;
-while(count < 10)
-{
-int what = new Random().Next(0, 3);
-}
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("*");
 
 Console.SetCursorPosition(xc, yc);
-Console.WriteLine("+");
+Console.WriteLine("*");
 
 int x = xa, y = xb;
 int count = 0;
-while(count < 10)
-{
-int what = new Random().Next(0, 3);
-if (what == 0)
-{
-x = (x + xa) / 2;
-y = (y + ya) / 2;
-}
+while(count < 1000)
 
-{
-int what = new Random().Next(0, 3);
-if (what == 0)
-{
-x = (x + xa) / 2;
-y = (y + ya) / 2;
+{    
+    int what = new Random().Next(0, 3);
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
 
-}
-if (what == 1)
-{
-x = (x + xb) / 2;
-y = (y + yb) / 2;
-}
-if (what == 2)
-{
-x = (x + xс) / 2;
-y = (y + yс) / 2;
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine(" + ");
+    count = count + 1;
 }
